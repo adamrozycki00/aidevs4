6fc19584438c75033d83f9b2f8bcc286515af6a3
arozycki <arozycki@ebay.com>
2026-03-14 22:23:50 +0100

    Refactor tests

--- src/test/java/com/tenetmind/ai/aidevs/task01/Task01Test.java
+++ src/test/java/com/tenetmind/ai/aidevs/task01/Task01Test.java
@@ -4,12 +4,22 @@ import com.tenetmind.aidevs.domain.tasks.task01.Task01;
 import com.tenetmind.aidevs.domain.tasks.task01.extractor.DummyExtractor;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SuppressWarnings("unchecked")
 class Task01Test {
 
   @Test
-  void getAnswer() {
+  void shouldGetCorrectAnswerForDummyExtractor() {
+    // given Task 01 with DummyExtractor, which always returns "transport" tag for any input
     Task01 task01 = new Task01(new DummyExtractor());
-    var res = task01.getAnswer();
-    IO.println(res);
+
+    // when
+    var answer = (List<Object>) task01.getAnswer();
+
+    // then the answer should contain 31 people that fulfill all criteria ignoring the "transport" tag
+    assertThat(answer).hasSize(31);
   }
 }
